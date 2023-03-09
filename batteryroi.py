c0f906ac7d893d81f4f1778c7645ceb9562f68be
Manuel Klimek <klimek@google.com>
2023-03-09 13:56:20 +0100

    Add battery roi, some fixes.

--- batteryroi.py
+++ batteryroi.py
@@ -3,37 +3,39 @@ import math
 
 def returns(
     years,
-    initial_output_pa,
-    self_use,
+    contribution,
     inflation,
     sell_price,
     initial_buy_price,
-    module_decay,
+    efficiency,
+    battery_decay,
     ):
     """Generates returns for each month in the given number of years.
 
     Args:
         years: Number of years to calculate.
-        inital_output_pa: Output / year in kWh in the first year.
-        self_use: Fraction of output consumed by user, the rest is sold.
+        contribution: Overall energy taken from battery per year in kWh.
         inflation: Inflation rate, e.g. 0.02.
         sell_price: Sell price  / kWh in EUR, e.g. 0.082.
         initial_buy_price: Initial price / kWh to buy energy, e.g. 0.3.
-        module_decay: Module efficiency at the end of the period, e.g. 0.8.
+        initial_efficiency: Initial efficiency of the battery, e.g. 0.9.
+        battery_decay: Rate at which contribution is lower after the examined
+            period, e.g. 0.8.
     """
     for month in range(0, 12*years):
-        decay = (1 - month*(1-module_decay)/(12*years))
-        output = (initial_output_pa/12) * decay
+        decay = (1 - month*(1-battery_decay)/(12*years))
         buy_price = initial_buy_price * math.pow(1+inflation/12, month) 
-        saved = output * self_use * buy_price
-        sold = output * (1-self_use) * sell_price
-        yield saved + sold
+
+        sell_less = (contribution * decay / 12) * sell_price
+        buy_less = (contribution * decay / 12) * efficiency * buy_price
+
+        yield buy_less - sell_less
 
 def cashflow(
     price,
     returns,
     ):
-    """Generates a cashflow for a PV investment.
+    """Generates a cashflow for a battery investment.
     
     Args:
         price: Price of the installation (positive).
@@ -54,18 +56,18 @@ def currentvalue(
 
 def genreturns():
     return returns(
-        years = 25,
-        initial_output_pa = 900,
-        self_use = 0.25,
+        years = 15,
+        contribution = 200, 
         inflation = 0.02,
         sell_price = 0.082,
         initial_buy_price=0.3,
-        module_decay = 0.8,
+        efficiency = 0.95,
+        battery_decay = 0.8,
     )
 
 def gencashflow():
     return cashflow(
-        1600,
+        437,
         genreturns(),
     )
 
